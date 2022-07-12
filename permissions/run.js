9e2ebf9af119c27ec4ac03c78da145990c356d49
DamonJohnson <85825667+DamonJohnson@users.noreply.github.com>
2022-07-12 16:55:36 +1000

    runs routes

--- permissions/run.js
+++
@@ -1,7 +0,0 @@
-const UserModel = require("../database/user_model")
-
-function canViewRun(user, run) {
-  return user.role === 'admin' || run.user === user._id
-}
-
-module.exports = { canViewRun }
--- routes/runs.js
+++ routes/runs.js
@@ -1,16 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const RunModel = require('../database/runs_model')
+const RunModel = require("../database/runs_model")
 const { authUser, authRole } = require("../basicAuth")
-const { canViewRun } = require("../permissions/run")
-const { reduceRight } = require("underscore")
-
 
 router.get("/", authUser, authRole("admin"), async (req, res, next) => {
   try {
     res.status(200).send(await RunModel.find())
-  }
-  catch {
+  } catch {
     res.status(400)
     return res.send("You are not authorised to view these runs")
   }
@@ -20,42 +16,62 @@ router.post("/", authUser, authRole("admin"), async (req, res, next) => {
   const newRun = {
     area: req.body.area,
     name: req.body.name,
-    user: req.body._id
   }
   RunModel.create(newRun, (err, doc) => {
     if (err) {
-        res.status(422).send(err.message)
+      res.status(422).send(err.message)
     } else {
       res.status(201).send(doc)
-      }
-    })
+    }
+  })
 })
 
-// router.get("/_id", setRun, authUser, authGetRun, (req, res) => {
-//   res.json(req.run)
-// })
+router.get("/:id", setRun, authUser, (req, res) => {
+  try {
+    res.status(200).send(req.run)
+  } catch {
+    res.status(400)
+  }
+})
+
+router.delete("/:id", setRun, authUser, (req, res) => {
+  RunModel.deleteOne(req.run, (err, doc) => {
+    if (err) {
+      res.status(405).send
+    } else {
+      res.status(204).send("Deleted run")
+    }
+  })
+})
+
+router.put("/:id", authUser, (req, res, next) => {
+  RunModel.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name, area: req.body.area },
+    {new : true},
+    (err, doc) => {
+      if (err) {
+        res.status(400).send
+      } else {
+        res.status(200).send(doc)
+      }
+    }
+  )
+})
 
 function setRun(req, res, next) {
-  const runId = parseInt(req.params._id)
+  const runId = req.params.id
   if (runId) {
     RunModel.findById(runId, (err, doc) => {
       if (err) {
         res.status(404)
-        return res.send("You are not authorised to view this" )
+        return res.send({ error: err.message })
       } else {
         req.run = doc
         next()
       }
     })
   }
-}
-
-function authGetRun(req, res, next) {
-  if (!canViewRun(req.user, req.run)) {
-    res.status(401)
-    return res.send('You are not authorised to view this run')
-  }
-  next()
 }
 
 module.exports = router
